aa28003a6821110dd7fa8301976b5d30154e95ca
Belal Azzam <pilalmohy@gmail.com>
2018-02-07 23:40:21 +0200

    passing parms to event handlers

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ class App extends Component {
             {name: 'max', age:28},
             {name: 'belal', age:22},
             {name: 'mezo', age:24},
-        ]
+        ],
+
     }
 
-     switchNameHandler = () => {
-        console.log('was clicked');
+     switchNameHandler = (newName) => {
+        // DONT DO THIS this.state.persons[0].name = 'bebo';
+        this.setState({persons: [
+            {name: 'maximilian', age:28},
+            {name: newName, age:22},
+            {name: 'mezo', age:24},
+        ]})
     }
 
   render() {
@@ -21,9 +27,15 @@ class App extends Component {
         <div className="App">
         <h1>hello i am a react app</h1>
          <p>this is rly working</p>
-            <button onClick={this.switchNameHandler}>Switch Name</button>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > my hobbies: bedan </Person>
+            <button onClick={() => this.switchNameHandler('maximilian !!!') }>Switch Name</button>
+            <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age} />
+            <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this,'Max!!')}
+            > my hobbies: bedan </Person>
             <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
         </div>
     );
