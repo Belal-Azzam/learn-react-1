6432d5918d6fd9b19ddfa9d48d8bfd3bd8941982
Belal Azzam <pilalmohy@gmail.com>
2018-02-07 23:26:45 +0200

    added event handling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
+
+    state = {
+        persons: [
+            {name: 'max', age:28},
+            {name: 'belal', age:22},
+            {name: 'mezo', age:24},
+        ]
+    }
+
+     switchNameHandler = () => {
+        console.log('was clicked');
+    }
+
   render() {
     return (
         <div className="App">
         <h1>hello i am a react app</h1>
          <p>this is rly working</p>
-            <Person name="belal" age="28" />
-            <Person name="belal" age="22" > my hobbies: bedan </Person>
-            <Person name="belal" age="23"/>
+            <button onClick={this.switchNameHandler}>Switch Name</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > my hobbies: bedan </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
         </div>
     );
     //   return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'hi i am react app'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const person = (props) => {
     return (
+        <div>
             <p>i am a {props.name} and i am {props.age} years old!</p>
-    )
+            <p>{props.children}</p>
+        </div>
+            )
 };
 
 export default person;
